77d19f875c729dc2a539619c67d5dfc881b03e19
srushti674 <spatel674@myseneca.ca>
2025-02-12 01:12:19 -0500

    Added files

---
+++ pet-adopt-app/app/index.jsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+      }}
+    >
+      <Text style ={{
+        fontFamily:'outfit-bold',
+        fontSize:40
+      }}>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
