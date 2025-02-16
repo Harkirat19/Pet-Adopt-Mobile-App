fdc4476e98bb262c91ac9545093dab3f22e24416
srushti674 <spatel674@myseneca.ca>
2025-02-16 14:17:41 -0500

    Added new font and confirmed project run

--- pet-adopt-app/app/_layout.jsx
+++ pet-adopt-app/app/_layout.jsx
@@ -37,6 +37,7 @@ export default function RootLayout() {
     outfit: require("./../assets/fonts/Outfit-Regular.ttf"),
     "outfit-medium": require("./../assets/fonts/Outfit-Medium.ttf"),
     "outfit-bold": require("./../assets/fonts/Outfit-Bold.ttf"),
+    "outfit-semibold": require("./../assets/fonts/Outfit-SemiBold.ttf"),
   });
 
   return (
