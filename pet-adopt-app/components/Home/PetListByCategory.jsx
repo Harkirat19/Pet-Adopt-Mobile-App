e0c05868fe4e39dcf599e500ce6cfaf62538724f
pparmar25 <parmarpranjal2004@gmail.com>
2025-04-09 14:17:50 -0400

    pet display list implemented

--- pet-adopt-app/components/Home/PetListByCategory.jsx
+++ pet-adopt-app/components/Home/PetListByCategory.jsx
@@ -1,12 +1,44 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, FlatList, Text } from 'react-native'
+import React, { useState, useEffect } from 'react'
 import Category from './Category'
+import PetListItem from './PetListItem' 
+import { collection, getDocs, query, where } from 'firebase/firestore'
+import { db } from '../../firebaseConfig' 
 
 export default function PetListByCategory() {
+  const [petList, setPetList] = useState([]);
+  const [loader, setLoader] = useState(false);
+
+  const GetPetList = async (category) => {
+    setLoader(true);
+    setPetList([]);
+    const q = query(collection(db, 'Pets'), where('category', '==', category));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach(doc => {
+      setPetList(prev => [...prev, doc.data()]);
+    });
+    setLoader(false);
+  }
+
+  useEffect(() => {
+    GetPetList('Dogs');
+  }, []);
+
   return (
     <View>
-      
-      <Category/>
+      <Category category={(value) => GetPetList(value)} />
+
+      <FlatList
+        data={petList}
+        style={{ marginTop: 10 }}
+        horizontal={true}
+        refreshing={loader}
+        onRefresh={() => GetPetList('Dogs')}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => (
+          <PetListItem pet={item} />
+        )}
+      />
     </View>
   )
 }
